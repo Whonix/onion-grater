e114d9e6a4f9621c2e9198495436a95a432a2d98
troubadoour <trobador@riseup.net>
2015-03-07 22:31:41 +0000

    cleanup, comments

--- usr/lib/control-port-filter-python/cpfp.py
+++ usr/lib/control-port-filter-python/cpfp.py
@@ -28,14 +28,11 @@ import logging
 import signal
 import sys
 
-## gevent signal does not pass arguments.
-#def signal_sigterm_handler(signal, frame):
 def signal_sigterm_handler():
     server.stop()
     logger.info('Signal sigterm received. Exiting.')
     sys.exit(143)
 
-#def signal_sigint_handler(signal, frame):
 def signal_sigint_handler():
     server.stop()
     logger.info('Signal sigint received. Exiting.')
@@ -49,37 +46,37 @@ class UnexpectedAnswer(Exception):
         return "[UnexpectedAnswer] " + self.msg
 
 def do_request_real(request):
-    # check if tor socket exists
+    ## Check if tor socket exists
     if not os.path.exists(SOCKET):
         logger.critical('Tor socket: "%s" does not exist' % (SOCKET))
         return
 
-    # The "lie" implemented in cpfp-bash
+    ## The "lie" implemented in cpfp-bash
     if request == 'getinfo net/listeners/socks' and LIMIT_GETINFO_NET_LISTENERS_SOCKS:
         temp = '250-net/listeners/socks="127.0.0.1:9150"\n'
         logger.info('Lying: %s' % (temp.strip()))
         return(temp)
 
-    # Read authentication cookie
+    ## Read authentication cookie
     with open(AUTH_COOKIE, "rb") as f:
         rawcookie = f.read(32)
         hexcookie = binascii.hexlify(rawcookie)
 
-        # Connect to the real control port
+        ## Connect to the real control port
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.settimeout(10.0)
         sock.connect(SOCKET)
         readh = sock.makefile("r")
         writeh = sock.makefile("w")
 
-        # Authenticate
+        ## Authenticate
         writeh.write("AUTHENTICATE " + hexcookie + "\n")
         writeh.flush()
         answer = readh.readline()
         if not answer.strip() == "250 OK":
-          raise UnexpectedAnswer("AUTHENTICATE failed")
+            raise UnexpectedAnswer("AUTHENTICATE failed")
 
-        # Send the request
+        ## Send the request
         writeh.write(request + '\n')
         writeh.flush()
         answer = sock.recv(LIMIT_STRING_LENGTH)
@@ -87,10 +84,9 @@ def do_request_real(request):
         sock.close()
         return answer
 
-
 def do_request(request):
     logger.info('Request: %s' % (request.strip()))
-    # Catch innocent exceptions, will report error instead
+    ## Catch innocent exceptions, will report error instead
     try:
         answer = do_request_real(request)
         logger.info('Answer: %s' % (answer.strip()))
@@ -98,59 +94,65 @@ def do_request(request):
     except (IOError, UnexpectedAnswer) as e:
         logger.error(e)
 
-
 def handle(sock, address):
-    # Keep accepting requests
     fh = sock.makefile()
+    ## Keep accepting requests
     while True:
-        # Read in a newline terminated line
+        ## Read in a newline terminated line
         line = fh.readline()
         if not line:
             break
-        # Strip escaped chars and white spaces at beginning and end of string
+        ## Strip escaped chars and white spaces.
+        ## Convert to lowercase.
         request = line.lower().strip()
 
-        # Authentication request.
+        ## Authentication request.
         if request.startswith("authenticate"):
             # Don't check authentication, since only
             # safe requests are allowed
             fh.write("250 OK\n")
 
         elif DISABLE_FILTERING:
-            # Pass all requests
+            ## Pass all requests
             answer = do_request(request)
             fh.write(answer)
 
         elif request in WHITELIST:
-            # Filtering enabled
+            ## Filtering enabled
             answer = do_request(request)
             fh.write(answer)
 
         else:
-            # Everything else we ignore/block
+            ## Everything else we ignore/block
             fh.write("510 Request filtered\n")
             logger.info('Request: %s' % (request.strip()))
             logger.warning('Answer: 510 Request filtered "%s"' % (request))
 
-        # Ensure the answer was written
+        ## Ensure the answer was written
         fh.flush()
 
-    # Ensure all data was written
+    ## Ensure all data was written
     fh.flush()
 
 if __name__ == "__main__":
-    # Generate random user ID.
     pid = os.getpid()
-    #print pid
 
-    # Create logger
-    logging.basicConfig(filename='/var/log/control-port-filter-python.log', level=logging.NOTSET)
+    ## Create logger
+    ## Starts a TCP server
+    ##   Logger available levels:
+    ##    .info
+    ##    .warning
+    ##    .error
+    ##    .critical
+    ##    .debug
+    logging.basicConfig(filename='/var/log/control-port-filter-python.log',
+                        level=logging.NOTSET)
     logger = logging.getLogger(unicode(pid))
 
     gevent.signal(signal.SIGTERM, signal_sigterm_handler)
     gevent.signal(signal.SIGINT, signal_sigint_handler)
 
-    # Default control port filer configuration
+    ## Default control port filer configuration
     IP = '10.152.152.10'
     PORT =  9052
     SOCKET = '/var/run/tor/control'
@@ -158,10 +160,11 @@ if __name__ == "__main__":
     DISABLE_FILTERING = False
     LIMIT_STRING_LENGTH = 16384
     LIMIT_GETINFO_NET_LISTENERS_SOCKS = True
-    WHITELIST = ['signal newnym', 'getinfo net/listeners/socks', 'getinfo status/bootstrap-phase', \
-                'getinfo status/circuit-established', 'quit']
+    WHITELIST = ['signal newnym', 'getinfo net/listeners/socks', 
+                 'getinfo status/bootstrap-phase',
+                 'getinfo status/circuit-established', 'quit']
 
-    # Read and override configuration from files
+    ## Read and override configuration from files
     if os.path.exists('/etc/cpfpy.d/'):
         files = sorted(glob.glob('/etc/cpfpy.d/*'))
 
@@ -198,15 +201,15 @@ if __name__ == "__main__":
                                 k, value = line.split('=')
                                 AUTH_COOKIE = str(value.strip())
 
-            # Disable limit.
+            ## Disable limit.
             if LIMIT_STRING_LENGTH == -1:
               # "sock.recv()" requires an argument. 64 KB, arbitrary.
               LIMIT_STRING_LENGTH = 65536
 
             WHITELIST = RequestList.split(',')
-            # Remove last element (comma)
+            ## Remove last element (comma)
             WHITELIST.pop()
-            # Remove duplicates
+            ## Remove duplicates
             WHITELIST = list(set(WHITELIST))
 
         else:
@@ -217,19 +220,11 @@ if __name__ == "__main__":
         logger.warning('User configuration folder "/etc/cpfpy.d" does not exist.')
         logger.warning('Running with default configuration.')
 
-    # Catch server exceptions.
-    # Most likely one: "Address already in use" if control port filter running.
+    ## Catch server exceptions.
     try:
-        # Starts a TCP server
-        #   Logger available levels:
-        #    .info
-        #    .warning
-        #    .error
-        #    .critical
-        #    .debug
         logger.info("Trying to start Tor control port filter on IP %s port %s" % (IP, PORT))
-        # ACCEPT CONCURRENT CONNECTIONS.
-        # limit to 5 simultaneous connections.
+        ## ACCEPT CONCURRENT CONNECTIONS.
+        ## limit to 5 simultaneous connections.
         server = StreamServer((IP, PORT), handle, spawn=5)
 
         #print "Tor control port filter started, listening on IP %s port %s" % (IP, PORT)
